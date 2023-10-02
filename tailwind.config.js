d87c5ef978eea5a96e2fcb428a3bae38d0aad28e
Zach3rry59 <draks.nathan@gmail.com>
2023-10-02 10:12:16 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+  },
+  /*global require */
+  plugins: [
+    require("@tailwindcss/forms"),
+    require("@tailwindcss/aspect-ratio"),
+  ],
+};
